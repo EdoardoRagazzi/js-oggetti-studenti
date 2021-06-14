ae228825dd7aea20d8db67c0c6b3b367d3579d1b
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-14 17:28:27 +0200

    END

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-var nomeStudente = prompt('Inserisci nome tudente:');
-var cognomeStudente = prompt('Inserisci cognome studente:');
-var etaStudente = parseInt(prompt('Inserisci eta:'));
+// var nomeStudente = prompt('Inserisci nome tudente:');
+// var cognomeStudente = prompt('Inserisci cognome studente:');
+// var etaStudente = parseInt(prompt('Inserisci eta:'));
 
-// // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-var studente = {
-    nome: nomeStudente,
-    cognome: cognomeStudente,
-    eta: etaStudente
-};
+// // // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// var studente = {
+//     nome: nomeStudente,
+//     cognome: cognomeStudente,
+//     eta: etaStudente
+// }
 
-console.log(studente);
+// console.log(studente);
 
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-for (var x in studente) {
-    console.log(studente[x]);
-};
+// // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// for (var x in studente) {
+//     console.log(studente[x]);
+// }
 
 // Creare un array di oggetti di studenti.
 var studenti = [
@@ -31,8 +31,8 @@ var studenti = [
         age: 28,
     },
     {
-        firstname: 'Ulisse',
-        surname: 'Rosso',
+        firstName: 'Ulisse',
+        surName: 'Rosso',
         age: 23,
     }
 ];
@@ -40,24 +40,24 @@ var studenti = [
 console.log(studenti);
 
 for (var i = 0; i < studenti.length; i++) {
-    console.log(studenti[i].firstName);
-    console.log(studenti[i].surName);
-    document.getElementById('studente').innerHTML += studenti[i].firstName + ' ' + studenti[i].surName + ' ';
 
-};
+    console.log(studenti[i].firstName + ' ' + studenti[i].surName);
+
+    document.getElementById('studente').innerHTML += studenti[i].firstName + ' ' + studenti[i].surName + ' ';
+}
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, 
 
-var nomeNewStudente = prompt('Inserisci nome tudente:');
-var cognomeNewStudente = prompt('Inserisci cognome studente:');
-var etaNewStudente = parseInt(prompt('Inserisci eta:'));
+// var nomeNewStudente = prompt('Inserisci nome tudente:');
+// var cognomeNewStudente = prompt('Inserisci cognome studente:');
+// var etaNewStudente = parseInt(prompt('Inserisci eta:'));
 
-var newStudente = {
-    nome: nomeNewStudente,
-    cognome: cognomeNewStudente,
-    eta: etaNewStudente
-};
+// var newStudente = {
+//     firstName: nomeNewStudente,
+//     surName: cognomeNewStudente,
+//     age: etaNewStudente
+// };
 
-studenti.push(newStudente);
+// studenti.push(newStudente);
 
-console.log(studenti);
+// console.log(studenti);
