c51c610f5abc7cc17de7a58b47342f665cc33af8
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-14 16:03:50 +0200

    Stampare a schermo Ciclo For IN

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
+var nomeStudente = prompt('Inserisci nome tudente:');
+var cognomeStudente = prompt('Inserisci cognome studente:');
+var etaStudente = parseInt(prompt('Inserisci eta:'));
+
+var studente = {
+    nome: nomeStudente,
+    cognome: cognomeStudente,
+    eta: etaStudente
+};
+
+console.log(studente);
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for (var x in studente) {
+    console.log(studente[x]);
+}
